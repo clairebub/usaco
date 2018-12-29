f244e93f0fa64f32d848288523b22222aadcd944
Claire Tang <claire.smurfs@gmail.com>
2018-12-29 10:44:23 -0800

    use java BigInteger for dollar days

--- gold/src/DollarDayz.java
+++ gold/src/DollarDayz.java
@@ -1,36 +1,37 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 
 public class DollarDayz {
 
     static int N, K;
-    static long[][] answers;
+    static BigInteger[][] answers;
     public static void main(String[] args) throws Exception {
         Scanner fin = new Scanner(new File("dollardayz.in"));
         N = fin.nextInt();
         K = fin.nextInt();
-        answers = new long[N+1][K+1];
+        answers = new BigInteger[N+1][K+1];
         for (int i = 0; i <= N; i++) {
             for (int j = 0; j <= K; j++) {
-                answers[i][j] = -1;
+                answers[i][j] = null;
             }
         }
         nk(N, K);
         System.out.println(answers[N][K]);
     }
 
-    static long nk(int n, int k) {
+    static BigInteger nk(int n, int k) {
         if (k == 1) {
-            answers[n][k] = 1;
-            return 1;
-        }
-        if (answers[n][k] >= 0) {
+            answers[n][k] = BigInteger.ONE;
             return answers[n][k];
         }
-        long total = 0;
+        if (answers[n][k] != null) {
+            return answers[n][k];
+        }
+        BigInteger total = BigInteger.ZERO;
         // buying k dollar-ed item i times
         for (int i = 0; n >= i*k; i++) {
-            total += nk(n-i*k, k-1);
+            total = total.add(nk(n-i*k, k-1));
         }
         answers[n][k] = total;
         return answers[n][k];
