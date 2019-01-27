6f6eb026ba112c76f81accc28d5ab98e01fed4d8
Claire Tang <claire.smurfs@gmail.com>
2019-01-27 11:00:02 -0800

    Poetry dp debugging

--- gold/src/Poetry.java
+++ gold/src/Poetry.java
@@ -2,111 +2,148 @@ import java.io.*;
 import java.util.*;
 
 public class Poetry {
-    static int[] dp;
+    static long[] dp;
+
     public static void main(String[] args) throws Exception {
+        for (int i = 1; i <= 15; i++) {
+            long t1 = System.currentTimeMillis();
+            System.out.println("i=" + i);
+            run(i);
+            long t2 = System.currentTimeMillis();
+            System.out.println("ts=" + (t2-t1) + " ms");
+        }
+//        run(1);
+    }
+    public static void run(int ii) throws Exception {
+
         int N_words, M_lines, K_syllables;
-        Scanner fin = new Scanner(new File("poetry_gold_jan19/2.in"));
-        StringTokenizer st = new StringTokenizer(fin.nextLine());
+        String fin_name = String.format("data/poetry_gold_jan19/%d.in", ii);
+        String fout_name = String.format("data/poetry_gold_jan19/s_%d.out", ii);
+        BufferedReader br = new BufferedReader(new FileReader(fin_name));
+        PrintWriter pw = new PrintWriter(new File(fout_name));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
         N_words = Integer.parseInt(st.nextToken());
         M_lines = Integer.parseInt(st.nextToken());
         K_syllables = Integer.parseInt(st.nextToken());
-        dp = new int[K_syllables+1];
-        Arrays.fill(dp, -1);
 
+        // we keep track the following
         int word_syllables[] = new int[N_words];
-        int word_rhymes[] = new int[N_words];
-        HashMap<Integer, HashSet<Integer>> class_members = new HashMap<>();
-        HashMap<Integer, Integer> si_counts = new HashMap<>();
+        int word_class[] = new int[N_words];
+        HashMap<Integer, HashSet<Integer>> class_2_words = new HashMap<>();
+        HashMap<Integer, Integer> syllable_wc = new HashMap<>();
 
         for (int i = 0; i < N_words; i++) {
-            st = new StringTokenizer(fin.nextLine());
-            int si = Integer.parseInt(st.nextToken());
-            int ci = Integer.parseInt(st.nextToken());
-            word_syllables[i] = si;
-            word_rhymes[i] = ci;
-            int si_count = 0;
-            if (si_counts.containsKey(si)) {
-                si_count = si_counts.get(si);
+            st = new StringTokenizer(br.readLine());
+            int num_syl = Integer.parseInt(st.nextToken());
+            int cls = Integer.parseInt(st.nextToken());
+            word_syllables[i] = num_syl;
+            word_class[i] = cls;
+
+            if (syllable_wc.containsKey(num_syl)) {
+                syllable_wc.put(num_syl, syllable_wc.get(num_syl) + 1);
+            } else {
+                syllable_wc.put(num_syl, 1);
             }
-            si_count += 1;
-            si_counts.put(si, si_count);
-            if (!class_members.containsKey(ci)) {
-                class_members.put(ci, new HashSet<Integer>());
+            if (!class_2_words.containsKey(cls)) {
+                class_2_words.put(cls, new HashSet<>());
             }
-            class_members.get(ci).add(i);
+            class_2_words.get(cls).add(i);
         }
-        HashMap<String, Integer> poetry_rhymes = new HashMap<>();
+
+        // num_of_ways2(K_syllables, syllable_wc);
+        // ways for a line to end with a class
+        dp = new long[K_syllables+1];
+        Arrays.fill(dp, -1);
+        HashMap<Integer, Long> class_ways = new HashMap<>();
+        for (int cls : class_2_words.keySet()) {
+            long sum = 0;
+            for (int w : class_2_words.get(cls)) {
+//                sum += dp[K_syllables - word_syllables[w]];
+                long a = num_of_ways2(K_syllables - word_syllables[w], syllable_wc);
+                sum = (sum + a) % 1000000007;
+                check_overflow("67", sum);
+            }
+            class_ways.put(cls, sum);
+        }
+
+        HashMap<String, Integer> rhyme_linesCount = new HashMap<>();
         for (int i = 0; i < M_lines; i++) {
-            st = new StringTokenizer(fin.nextLine());
-            int num_lines = 0;
+            st = new StringTokenizer(br.readLine());
             String a = st.nextToken();
-            if (poetry_rhymes.containsKey(a)) {
-                num_lines = poetry_rhymes.get(a);
+            if (rhyme_linesCount.containsKey(a)) {
+                rhyme_linesCount.put(a, rhyme_linesCount.get(a) + 1);
+            } else {
+                rhyme_linesCount.put(a, 1);
             }
-            num_lines += 1;
-            poetry_rhymes.put(a, num_lines);
         }
-        // we need to know for each rhyme class, the list of words for that rhyme class
-        // then, we need to know the number of ways per rhyme class
-//        System.out.println("class_members: " + class_members);
-        HashMap<Integer, Integer> class_ways = new HashMap<>();
-        for (int ci : class_members.keySet()) {
-            int ways = 0;
-            for (int w : class_members.get(ci)) {
-                int a = word_syllables[w];
-                int x = num_of_ways(K_syllables - a, si_counts);
-                ways += x;
-            }
-            class_ways.put(ci, ways);
-        }
-        int total = 1;
-        for (String r : poetry_rhymes.keySet()) {
-            int line_count = poetry_rhymes.get(r);
-            // how many ways for this rhyme
-            int x = 0;
-            for (int c : class_ways.keySet()) {
-                int w = class_ways.get(c);
-                int t = 1;
-                for (int i = 0; i < line_count; i++) {
-                    t *= w;
-                    t = t % 1000000007;
+        long total = 1;
+        for (String r : rhyme_linesCount.keySet()) {
+            long r_ways = 0; // contributions from this rhyme
+            int lines = rhyme_linesCount.get(r);
+            for (int cls : class_ways.keySet()) {
+                long ways_from_cls = 1;
+                long ways = class_ways.get(cls);
+                for (int i = 0; i < lines; i++) {
+                    ways_from_cls =  (ways_from_cls * ways) % 1000000007;
                 }
-                x += t;
+                r_ways = (r_ways + ways_from_cls) % 1000000007;
+                check_overflow("94", r_ways);
             }
-            total *= x;
-            total = total % 1000000007;
+            total = total * r_ways % 1000000007;
         }
 
-        //
-        //PrintWriter fout = new PrintWriter(new File("poetry.out"));
-        PrintWriter fout = new PrintWriter(System.out);
-     //   fout.println(String.format("N=%d, M=%d, K=%d", N_words, M_lines, K_syllables));
-     //   fout.println(Arrays.toString(word_syllables));
-     //   fout.println(Arrays.toString(word_rhymes));
-        fout.println(total);
-
-        fout.flush();
-        fout.close();
+        System.out.println("total=" + total);
+        pw.println(total);
+        pw.flush();
+        pw.close();
     }
 
-    // number of unique ways to form a single line of K syllables
-    static int num_of_ways(int syllables, HashMap<Integer, Integer> syllable_counts) {
-        // go through each words,
-        if (dp[syllables] >= 0) {
-            return dp[syllables];
+    static long num_of_ways2(int K, HashMap<Integer, Integer> syllable_length_to_count) {
+        if (dp[K] >= 0) {
+            return dp[K];
         }
-        int sum = 0;
-        for (int num_syllables : syllable_counts.keySet()) {
-            if (num_syllables == syllables) {
-                sum += syllable_counts.get(num_syllables);
-            } else if (num_syllables < syllables) {
-                sum += num_of_ways(syllables - num_syllables, syllable_counts) * syllable_counts.get(num_syllables);
-            } else {
-                // not fit any more
+        for (int i = 1; i <= K; i++) {
+            long sum = 0;
+            for (int j = 1; j < i; j++) {
+                if (dp[j] == 0 || dp[i-j] == 0) {
+                    continue;
+                }
+                long a =  (dp[j]) * (dp[i-j]) % 1000000007;
+                String m = String.format("i=%d, j=%d, i-j=%d, dp[j]=%d, dp[i-j]=%d, a=%d", i, j, i-j, dp[j], dp[i-j], a);
+                check_overflow(m, a);
+                sum = (sum + a) % 1000000007;
             }
+            sum += syllable_length_to_count.containsKey(i) ? syllable_length_to_count.get(i) : 0;
+            sum = sum % 1000000007;
+            dp[i] = sum;
         }
-        dp[syllables] = sum;
-//        System.out.println("dp[" + syllables + "] = " + sum);
-        return dp[syllables];
+        return dp[K];
+    }
+/*
+    // number of unique ways for K syllables
+    static long num_of_ways(int num_s, HashMap<Integer, Integer> syllable_length_to_count) {
+        if (dp[num_s] >= 0) {
+            return dp[num_s];
+        }
+        long sum = 0;
+        for (int len : syllable_length_to_count.keySet()) {
+            int c = syllable_length_to_count.get(len);
+            if (len == num_s) {
+                sum += c;
+            } else if (len < num_s) {
+                long a = c * num_of_ways(num_s - len, syllable_length_to_count) % 1000000007;
+                sum = (sum + a) % 1000000007;
+            }
+            sum = sum % 1000000007;
+        }
+        dp[num_s] = sum;
+        return dp[num_s];
+    }
+*/
+    static void check_overflow(String marker, long n) {
+        if (n < 0) {
+            System.out.println(marker + " overflow: " + n);
+        }
     }
 }
