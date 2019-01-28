31d83532d6644e7c9696617fa5d9581e600ad4d6
Claire Tang <claire.smurfs@gmail.com>
2019-01-27 21:35:55 -0800

    cowpatibility 100% with max at 2.5 seconds

--- gold/src/Dec18_Cowpatibility2.java
+++ gold/src/Dec18_Cowpatibility2.java
@@ -3,8 +3,44 @@ import java.util.*;
 
 public class Dec18_Cowpatibility2 {
   static int N;
-  static HashMap<String, Integer> compat = new HashMap<>();
-  static int[] index2Keysize = new int[32];
+  static HashMap<MatchKey, Integer> compat = new HashMap<>();
+
+  static class MatchKey {
+    public  MatchKey() {
+      n = 0;
+      Arrays.fill(flavors, 0);
+    }
+
+    int n;
+    int[] flavors = new int[5];
+
+    @Override
+    public boolean equals(Object obj) {
+      if (!(obj instanceof MatchKey)) {
+        return false;
+      }
+      MatchKey mk = (MatchKey) obj;
+      if (mk.n != n) {
+        return false;
+      }
+      for (int i = 0; i < n; i++) {
+        if (flavors[i] != mk.flavors[i]) {
+          return false;
+        }
+      }
+      return true;
+    }
+
+    @Override
+    public int hashCode() {
+      int result = 17;
+      result = 37 * result + n;
+      for (int i = 0; i < n; i++) {
+        result = 37 * result + flavors[i];
+      }
+      return result;
+    }
+  }
 
   public static void main(String[] args) throws Exception {
     run(args.length == 0 ? 0 : Integer.parseInt(args[0]));
@@ -23,7 +59,6 @@ public class Dec18_Cowpatibility2 {
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     int[] flavors = new int[5];
-    int[] taken = new int[5];
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
       for (int j = 0; j < 5; j++) {
@@ -31,59 +66,39 @@ public class Dec18_Cowpatibility2 {
       }
       Arrays.sort(flavors);
       for (int j = 1; j <= 31; j++) {
-        String k = gen_subset(flavors, taken, j);
-        if (!compat.containsKey(k)) {
-          compat.put(k, 1);
-        } else {
-          compat.put(k, compat.get(k) + 1);
-        }
+        MatchKey mk = gen_subset(flavors, j);
+        int count = compat.containsKey(mk) ? compat.get(mk) : 0;
+        compat.put(mk, ++count);
       }
     }
-
+    
     // get number of compatible pairs using inclusion-exclusion
     long[] inc_ex = {0, 1, -1, 1, -1, 1};
     long total = 1L * N * (N-1) / 2;
-    for (Map.Entry<String, Integer> entry : compat.entrySet()) {
+    for (Map.Entry<MatchKey, Integer> entry : compat.entrySet()) {
       long matched_people = entry.getValue();
       if (matched_people == 1) {
         continue;
       }
       long matched_pairs = matched_people * (matched_people - 1) / 2;
-      st = new StringTokenizer(entry.getKey(), ",");
-      int n = Integer.parseInt(st.nextToken());
-      total -= inc_ex[n] * matched_pairs;
+      total -= inc_ex[entry.getKey().n] * matched_pairs;
     }
+
     pw.println(total);
     pw.flush();
     pw.close();
   }
 
 
-  static void gen_index(int index) {
-    int n = 0;
+  static MatchKey gen_subset(int[] flavors, int index) {
+    MatchKey mk = new MatchKey();
+    mk.n = 0;
     for (int i = 0; i < 5; i++) {
       if (((1 << i) & index) != 0) {
-        n++;
+        mk.flavors[mk.n] = flavors[i];
+        mk.n++;
       }
     }
-    index2Keysize[index] = n;
-  }
-
-  // pivot, subscribe the cow into each flavor set it might find its compatible mate in.
-  static String gen_subset(int[] flavors, int[] taken, int index) {
-    int n = 0;
-    for (int i = 0; i < 5; i++) {
-      if (((1 << i) & index) != 0) {
-        taken[n] = flavors[i];
-        n++;
-      }
-    }
-
-    StringBuilder sb = new StringBuilder("" + n);
-    for (int i = 0; i < n; i++) {
-      sb.append(",");
-      sb.append(taken[i]);
-    }
-    return sb.toString();
+    return mk;
   }
 }
