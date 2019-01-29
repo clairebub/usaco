c8ff598f21a1239bb2f0a809443703b1665799c3
Claire Tang <claire.smurfs@gmail.com>
2019-01-28 21:24:34 -0800

    clean up the class and outfiles in gitignore

---
+++ gold/src/Open18_Sort.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Open18_Sort {
+  public static void main(String[] args) throws Exception {
+    run(args.length == 0 ? 0 : Integer.parseInt(args[0]));
+  }
+
+  static void run(int ii) throws Exception {
+    String fin_name = 
+  }
+}
